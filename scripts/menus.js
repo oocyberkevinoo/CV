ff1d074a1ac8ea16c81cf1d4c3df5326cde72919
CyberKevin <cyberkevin13140@hotmail.fr>
2022-07-01 21:57:18 +0200

    new menus, new pages to test stuff

--- scripts/menus.js
+++ scripts/menus.js
@@ -17,30 +17,56 @@ function showMenu(element){
 
 	
 	if(val == "true"){
-		if(element == "softwares"){
-			document.getElementById("menu-"+lang+element).innerHTML= "<a href='./?page=software/index'>- Présentation Globale</a>"+
-		"<a href='./?page=software/1'>- jeu 1</a>"+
-		"<a href='./?page=software/2'>- jeu 2</a>"+
-		"<a href='./?page=software/3'>- jeu 3</a>";
-		}	
-		if(element == "games"){
-			document.getElementById("menu-"+lang+element).innerHTML= "<a href='./?page=game/index'>- Présentation Globale</a>"+
-		"<a href='./?page=game/1'>- jeu 1</a>"+
-		"<a href='./?page=game/2'>- jeu 2</a>"+
-		"<a href='./?page=game/3'>- jeu 3</a>";
-		}	
-		if(element == "vrar"){
-			document.getElementById("menu-"+lang+element).innerHTML= "<a href='./?page=vrar/index'>- Présentation Globale</a>"+
-		"<a href='./?page=vrar/1'>- jeu 1</a>"+
-		"<a href='./?page=vrar/2'>- jeu 2</a>"+
-		"<a href='./?page=vrar/3'>- jeu 3</a>";
-		}	
-		if(element == "hardwares"){
-			document.getElementById("menu-"+lang+element).innerHTML= "<a href='./?page=hardware/index'>- Présentation Globale</a>"+
-		"<a href='./?page=hardware/1'>- jeu 1</a>"+
-		"<a href='./?page=hardware/2'>- jeu 2</a>"+
-		"<a href='./?page=hardware/3'>- jeu 3</a>";
-		}	
+		if(lang == "FR"){
+			if(element == "softwares"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=software/index'>- Présentation Globale</a>"+
+			"<a href='./?page=software/1'>- jeu 1</a>"+
+			"<a href='./?page=software/2'>- jeu 2</a>"+
+			"<a href='./?page=software/3'>- jeu 3</a></div>";
+			}	
+			if(element == "games"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=game/index'>- Présentation Globale</a>"+
+			"<a href='./?page=game/1'>- jeu 1</a>"+
+			"<a href='./?page=game/2'>- jeu 2</a>"+
+			"<a href='./?page=game/3'>- jeu 3</a></div>";
+			}	
+			if(element == "vrar"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=vrar/index'>- Présentation Globale</a>"+
+			"<a href='./?page=vrar/1'>- Hologram VR Show</a>"+
+			"<a href='./?page=vrar/2'>- Prototypes HandTracking</a>"+
+			"<a href='./?page=vrar/3'>- Prototype Multijoueur à un casque VR</a></div>";
+			}	
+			if(element == "hardwares"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=hardware/1'>- Controlleur Arcade ARDUINO</a></div>";
+			}	
+		}
+		else if(lang == "EN"){
+			if(element == "softwares"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=software/index'>- Global Presentation</a>"+
+			"<a href='./?page=software/1'>- game 1</a>"+
+			"<a href='./?page=software/2'>- game 2</a>"+
+			"<a href='./?page=software/3'>- game 3</a></div>";
+			}	
+			if(element == "games"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=game/index'>- Global Presentation</a>"+
+			"<a href='./?page=game/1'>- game 1</a>"+
+			"<a href='./?page=game/2'>- game 2</a>"+
+			"<a href='./?page=game/3'>- game 3</a></div>";
+			}	
+			if(element == "vrar"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=vrar/index'>- Global Presentation</a>"+
+			"<a href='./?page=vrar/1'>- game 1</a>"+
+			"<a href='./?page=vrar/2'>- game 2</a>"+
+			"<a href='./?page=vrar/3'>- game 3</a></div>";
+			}	
+			if(element == "hardwares"){
+				document.getElementById("menu-"+lang+element).innerHTML= "<div class='w3-animate-left w3-theme-d2'><a href='./?page=hardware/index'>- Global Presentation</a>"+
+			"<a href='./?page=hardware/1'>- game 1</a>"+
+			"<a href='./?page=hardware/2'>- game 2</a>"+
+			"<a href='./?page=hardware/3'>- game 3</a></div>";
+			}	
+		}
+		
 	}
 	else if(val == "" || val == "false" || val == null)
 			document.getElementById("menu-"+lang+element).innerHTML="";
